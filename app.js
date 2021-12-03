697c0f01fdac73709b5d04d83d93649010962f9f
Manish Raj <rajmanish6851@gmail.com>
2021-12-03 08:51:32 +0530

    Used the concept of sync and async

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ const rqListener = (req, res) => {
   if (url === "/message" && method === "POST"){
       const body = []
       req.on('data', (chunk) => {
-          console.log(chunk)
           body.push(chunk)
       })
-      req.on('end', () => {
+      return req.on('end', () => {
           const parsedBody = Buffer.concat(body).toString()
           const message = parsedBody.split('=')[1]
-          fs.writeFileSync('message.txt', message)
-          console.log(parsedBody)
+          // fs.writeFileSync('message.txt', message) block the next line until this is completed
+          fs.writeFile('message.txt', message, (error) => {
+            res.statusCode = 302
+            res.setHeader('Location', '/')
+            return res.end()
+          })
       })
-      res.statusCode = 302
-      res.setHeader('Location', '/')
-      return res.end()
   }
   res.setHeader("Content-Type", "text/html");
   res.write(
